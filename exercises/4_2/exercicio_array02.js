085943ab62159192a74c7a5da4608185d6879046
Antonio Fernandes Lima <limant@gmail.com>
2020-07-14 15:39:00 -0300

    Subindo exe 01 e 02 ARRAY e FOR

---
+++ exercises/4_2/exercicio_array02.js
@@ -0,0 +1,11 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+//Para o segundo exercício, você deve somar todos os valores contidos no array e imprimir o resultado;
+
+
+var total = 0;
+for (var index = 0; index < numbers.length; index++){  
+    total = total + numbers[index];
+    }
+console.log(total);
+

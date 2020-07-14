138a6034ef802a9405ac72aa75b6ba5c939dd315
Antonio Fernandes Lima <limant@gmail.com>
2020-07-14 11:49:32 -0300

    Exercicio 01 e 02

---
+++ exercises/4_1/exercicio01.js
@@ -0,0 +1,16 @@
+//<!-- Exercicio 01 -->
+let a = 10;
+let b = 8;
+
+resultado = a + b;
+console.log(resultado);
+
+resultado = a - b;
+console.log(resultado);
+
+resultado = a * b
+console.log(resultado);
+
+resultado = a / b;
+console.log(resultado);
+
---
+++ exercises/4_1/exercicio02.js
@@ -0,0 +1,10 @@
+//<!-- Exercicio 02 -->
+
+num1 = 2;
+num2 = 5;
+
+if (num1 > num2){
+    console.log(num1 + " e maior que " + num2);
+}else{
+    console.log(num1 + ' nao e maior ' +num2);
+}

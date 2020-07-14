265f8f23bf60976f7edd37e5e33c262ce4160985
Antonio Fernandes Lima <limant@gmail.com>
2020-07-14 12:16:44 -0300

    Subindo exercicio 04

---
+++ exercises/4_1/exercicio04.js
@@ -0,0 +1,11 @@
+//Faça um programa que, dado um valor definido numa variável, retorne "positive" se esse valor for positivo, "negative" se for negativo e "zero" caso contrário.
+
+let valor = -2;
+
+if (valor > 0){
+    console.log("O valor e positivo");
+}else if (valor < 0){
+    console.log ("O valor e negativo");
+}else{
+    console.log(" O valor e zero");
+}
